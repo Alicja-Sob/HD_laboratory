fcf8629777cef927a86ac5adc805bfba9d33ca28
Alicja-Sob <102226341+Alicja-Sob@users.noreply.github.com>
2025-11-26 21:00:09 +0100

    TSQL script for Date ETL

---
+++ ETL/static_DATA.sql
@@ -0,0 +1,31 @@
+USE HurtowniaDanychRel
+GO
+
+-- DECLARING START AND END DATES
+
+DECLARE @EarliestDate date;	-- for dates of birth etc
+
+DECLARE @StartDateT1 date;
+DECLARE @EndDateT1 date;
+
+DECLARE @StartDateT2 date;
+DECLARE @EndDateT2 date;
+
+SELECT @EarliestDate ='1937-01-01', @StartDateT1 = '2005-01-01', @EndDateT1 = '2020-12-31', @StartDateT2 = '2021-02-01', @EndDateT2 = '2025-12-31';
+
+-- WHILE LOOP
+
+DECLARE @DateInProcess date = @EarliestDate
+
+-- loop is for both snapshots at once, idk if that's how it's supposed to be for the lab
+WHILE @DateInProcess <= @EndDateT2	
+	BEGIN
+		INSERT INTO [dbo].[_Data] ( [Dzien], [Miesiac], [Rok])
+		VALUES (
+			CAST (Year(@DateInProcess) as INT),
+			CAST (Month(@DateInProcess) as INT),
+			CAST (DatePart(dw, @DateInProcess) as INT)
+		);
+		SET @DateInProcess = DateAdd(d, 1, @DateInProcess);
+	END
+GO
